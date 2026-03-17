e30d2ec5605d28616231be3ab587b766a6217a41
slargo <santiagolargo0118@gmail.com>
2026-03-17 13:31:44 +0100

    listo

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
 puntos = 100
+nombre = "gonorea"
+print(f"tienes {puntos} {nombre} ome")
+print(f"tienes {puntos} gonorrea ome")
+print(f"tienes {puntos} gonorrea ome")
+print(f"tienes {puntos} gonorrea ome")
+print(f"tienes {puntos} gonorrea ome")
+print(f"tienes {puntos} gonorrea ome")
 print(f"tienes {puntos} gonorrea ome")
